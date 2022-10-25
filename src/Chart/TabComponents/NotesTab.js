ca5eb21bcb985d59e27b7264ed8715360450f7ff
abhigyanzemoso <abhigyan.pandey@zemosolabs.com>
2022-10-25 13:30:01 +0530

    create-note-and-remaining-inconsistencies

--- src/Chart/TabComponents/NotesTab.js
+++ src/Chart/TabComponents/NotesTab.js
@@ -1,22 +1,138 @@
-import { Grid, Paper, Button, Drawer } from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Button,
+  Drawer,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import { useTheme } from "@mui/system";
 import { useEffect, useState } from "react";
 import { NoteService } from "../../services/P360/noteService";
 import CreateNotes from "../CreateNotes";
 
-const NotesTab = ({ patientId }) => {
+const NotesTab = ({ patientDetails }) => {
   const theme = useTheme();
   const [notes, setNotes] = useState([]);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [notesPayload, setNotesPayload] = useState({
+    data: {
+      resourceType: "DocumentReference",
+      status: "current",
+      subject: {
+        reference: `Patient/${patientDetails?.id}`,
+      },
+      category: [
+        {
+          coding: [
+            {
+              system: "https://sandbox.elationemr.com/api/2.0/visit_note_types",
+              code: "Elation",
+              display: "Simple",
+            },
+          ],
+        },
+      ],
+      docStatus: "preliminary",
+      date: "",
+      extension: [
+        {
+          url: "http://xcaliber-fhir/structureDefinition/practice",
+          valueString: 140857911017476,
+        },
+        {
+          url: "http://xcaliber-fhir/structureDefinition/created-date",
+          valueString: "2022-09-17T07:32:16Z",
+        },
+      ],
+      context: {
+        encounter: [
+          {
+            reference: "Encounter/2022-09-17T07:32:16Z",
+          },
+        ],
+        period: {
+          start: "2022-09-17T07:32:16Z",
+        },
+      },
+      author: [
+        {
+          reference: "Practitioner/140857915539458",
+          identifier: {
+            id: "user_id",
+            value: 2758,
+          },
+        },
+      ],
+    },
+  });
 
-  const getNotes = async () => {
+  const getNotes = async (patientId) => {
     const result = await NoteService.getVisitNotes(patientId);
     setNotes(result);
   };
 
+  // patientDetails,
+  // noteFormDetails,
+  const onCreateClick = async (notesPayload) => {
+    await NoteService.createNote(notesPayload);
+    setIsDrawerOpen(false);
+  };
+
+  const onCancelClick = () => {
+    setIsDrawerOpen(false);
+  };
+  const onDateChange = (date) => {
+    setNotesPayload({
+      data: {
+        ...notesPayload?.data,
+        date: date,
+      },
+    });
+  };
+  const onTimeChange = (time) => {
+    let finalDateValue = notesPayload?.data?.date?.slice(0, 10);
+    setNotesPayload({
+      data: {
+        ...notesPayload?.data,
+        start: `${finalDateValue}${time}`,
+      },
+    });
+  };
+  const onTemplateChange = (templateValue) => {
+    setNotesPayload({
+      data: {
+        ...notesPayload?.data,
+        category: [
+          {
+            coding: [
+              {
+                system:
+                  "https://sandbox.elationemr.com/api/2.0/visit_note_types",
+                code: "Elation",
+                display: templateValue,
+              },
+            ],
+          },
+        ],
+      },
+    });
+  };
+  const updatePatientId = (patientId) => {
+    setNotesPayload({
+      data: {
+        ...notesPayload?.data,
+        subject: {
+          reference: `Patient/${patientId}`,
+        },
+      },
+    });
+  };
+
   useEffect(() => {
-    Promise.all([getNotes()]);
+    Promise.all([getNotes(patientDetails?.id)]);
   }, []);
+
   return (
     <Grid container spacing={2} direction={"column"}>
       <Button
@@ -47,10 +163,31 @@ const NotesTab = ({ patientId }) => {
           },
         }}
       >
-        <CreateNotes />
+        <CreateNotes
+          noteFormDetails={notesPayload}
+          onCancelClick={onCancelClick}
+          onDateChange={onDateChange}
+          onTimeChange={onTimeChange}
+          onCreateClick={onCreateClick}
+          onTemplateChange={onTemplateChange}
+          updatePatientId={updatePatientId}
+        />
       </Drawer>
       <Paper style={{ marginTop: theme.spacing(4), overflow: "scroll" }}>
-        {notes}
+        {notes &&
+          notes?.map((note) => {
+            return (
+              <ListItemButton
+                style={{ flexDirection: "column", alignItems: "flex-start" }}
+                divider
+                key={note.id}
+              >
+                <ListItemText primary={note?.resource?.description} />
+                <ListItemText secondary={note?.resource?.docStatus} />
+                <ListItemText secondary={note?.resource?.date} />
+              </ListItemButton>
+            );
+          })}
       </Paper>
     </Grid>
   );
--- src/services/P360/noteService.js
+++ src/services/P360/noteService.js
@@ -5,7 +5,27 @@ export const NoteService = {
   getVisitNotes: async (patientId) => {
     try {
       const response = await axios.get(
-        `${XCHANGE_SERVICE_ENDPOINT}//api/v1/DocumentReference?patient=140919926030337&type=visit-notes&_count=3`,
+        `${XCHANGE_SERVICE_ENDPOINT}/api/v1/DocumentReference?patient=${patientId}&type=visit-notes&_count=3`,
+        {
+          headers: {
+            Authorization: `${process.env.REACT_APP_AUTHORIZATION}`,
+            "x-source-id": `${process.env.REACT_APP_XSOURCEID}`,
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          },
+        }
+      );
+      return response.data?.data?.entry;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  createNote: async (notePayload) => {
+    try {
+      console.log(notePayload);
+      const response = await axios.post(
+        `${XCHANGE_SERVICE_ENDPOINT}/api/v1/DocumentReference?type=visit-notes`,
+        notePayload,
         {
           headers: {
             Authorization: `${process.env.REACT_APP_AUTHORIZATION}`,
