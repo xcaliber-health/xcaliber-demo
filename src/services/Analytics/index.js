e9a25647bae4d816ca5e7a425c17567a2335df4b
lavanya-mandapati <lavanya.mandapati@zemosolabs.com>
2022-11-03 18:18:41 +0530

    analytics data

--- src/services/Analytics/index.js
+++ src/services/Analytics/index.js
@@ -3,17 +3,15 @@ import { LENS_API_ENDPOINT } from "../../core-utils/constants";
 
 export const AnalyticService = {
   discoverLens: async (payload) => {
-    console.log(typeof(payload));
-    console.log(typeof({"query": "SELECT * from Clinical_Summary_Lens where Patient_ID = '140927993708545' AND Immunization_Code = '213' LIMIT 100"}));
     try {
       let url = `${LENS_API_ENDPOINT}/api/v1/datalenses/5197fc6c-b44a-4d94-87f0-9e09aa27bfc3/discover`;
-      const response = await axios.post(url,JSON.parse(payload), {
+      const response = await axios.post(url, JSON.parse(payload), {
         headers: {
           Authorization: `${process.env.REACT_APP_AUTHORIZATION}`
         },
       });
       console.log(response);
-      return response.data?.results?.slice(0,2);
+      return response.data?.results?.slice(0, 2);
     } catch (error) {
       //Handle Exception
       console.log(error);
