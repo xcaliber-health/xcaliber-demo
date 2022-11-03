7b69d2c1a92a28031ebb1f17f17f5bba7fa2ed8c
lavanya-mandapati <lavanya.mandapati@zemosolabs.com>
2022-11-03 18:42:09 +0530

    fix

--- src/Analytics/index.js
+++ src/Analytics/index.js
@@ -6,33 +6,33 @@ import { AnalyticService } from "../services/Analytics";
 import LensTable from "./LensTable";
 
 export default function Terminology() {
-  let result=[]
+  let result = []
   const theme = useTheme();
   const [lensData, setLensData] = useState([]);
-  const [flag,setFlag]=useState(0);
-  const [data,setData]=useState([
-    {Allergies:[]},
-    {FamilyHistory:[]},
-    {Immunizations:[]},
-    {Insurance:[]},
-    {Medications:[]},
-    {Patient:{}},
-    {Patient_ID:''},
-    {Problems:[]},
-    {Procedures:[]},
-    {Vitals:[]}
-])
+  const [flag, setFlag] = useState(0);
+  const [data, setData] = useState([
+    { Allergies: [] },
+    { FamilyHistory: [] },
+    { Immunizations: [] },
+    { Insurance: [] },
+    { Medications: [] },
+    { Patient: {} },
+    { Patient_ID: '' },
+    { Problems: [] },
+    { Procedures: [] },
+    { Vitals: [] }
+  ])
   // const [tableData,setTableData]=useState([
-    
+
   // ]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    if (data && data!== null) {
-     setLoading(false);
+    if (data && data !== null) {
+      setLoading(false);
     }
   }, [data]);
   return (
-    <Grid width={"80%"} height={`100%`} container direction="column">
+    <Grid container direction="column">
       <Grid
         container
         item
@@ -40,10 +40,10 @@ export default function Terminology() {
         sx={{ marginBottom: theme.spacing(2) }}
       >
         <Grid item>
-          <TextField label="query" size="small" sx={{marginRight:"20px"}}value={lensData}
-          onChange={(e)=>{setLensData(e.target.value)}}/>
+          <TextField label="query" size="small" sx={{ marginRight: "20px", width: "1000px" }} value={lensData}
+            onChange={(e) => { setLensData(e.target.value) }} />
         </Grid>
-       
+
         <Grid item>
           <Button
             onClick={async () => {
@@ -52,7 +52,7 @@ export default function Terminology() {
               result = await AnalyticService.discoverLens(lensData);
               setData(result);
               setFlag(1);
-              
+
             }}
             variant="contained"
           >
@@ -63,7 +63,7 @@ export default function Terminology() {
 
       <Grid item>
         {
-        !loading && <LensTable tableRowData={data} flag={flag} />}
+          !loading && <LensTable tableRowData={data} flag={flag} />}
         {loading && "Searching"}
       </Grid>
     </Grid>
