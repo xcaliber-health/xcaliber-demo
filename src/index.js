683b306b8e188912c2c5a9c970f3ab63268aeb10
saumitra403 <mishra.saumitra06@gmail.com>
2023-06-12 21:44:09 +0530

    Popup changes

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
 import { Provider } from "react-redux";
+import PopupHOC from "./Popup/PopupHOC";
 import store from "./Redux/store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
+const EnhancedApp = PopupHOC(App);
 root.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <App />
+      <EnhancedApp></EnhancedApp>
     </ThemeProvider>
   </Provider>
 );
