cc200654bb26896ca2a981e9374d58c7f7e4388e
tanishaxcaliber <tanisha.shrivas@xcaliber.health>
2025-01-08 22:14:57 +0530

    [TS]: Allergies card

---
+++ packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/AllergiesDetails.tsx
@@ -0,0 +1,168 @@
+import tableStyles from "@core/styles/table.module.css";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Typography from "@mui/material/Typography";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useMemo, useState } from "react";
+// React Icons
+import { FaEye, FaPen } from "react-icons/fa";
+// import { fetchAllergies } from "./utils/getPatientAllergies";
+
+interface AllergyProps {
+  allergy: string;
+  status: string;
+  description: string;
+  last_updated: string;
+  action: string;
+}
+
+const allergyDetails = [
+  {
+    allergy: "Name",
+    status: "Active",
+    description: "asdf",
+    last_updated: "Tue Dec 31 2024, 04:00 PM",
+    action: "view/edit",
+  },
+  {
+    allergy: "Name",
+    status: "Active",
+    description: "asdf",
+    last_updated: "Tue Dec 31 2024, 04:00 PM",
+    action: "view/edit",
+  },
+  {
+    allergy: "Name",
+    status: "Inactive",
+    description: "asdf",
+    last_updated: "Tue Dec 31 2024, 04:00 PM",
+    action: "view/edit",
+  },
+];
+
+const AllergiesTable = ({ id }: { id?: string }) => {
+  const [rowSelection, setRowSelection] = useState({});
+  const [data, setData] = useState<AllergyProps[]>([...allergyDetails]);
+
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       try {
+  //         const response = await fetchProblems(id);
+  //         setData(response);
+  //       } catch (error) {
+  //         console.error("Error fetching problems:", error);
+  //       }
+  //     };
+
+  //     fetchData();
+  //   }, [id]);
+
+  const columnHelper = createColumnHelper<AllergyProps>();
+
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("allergy", {
+        header: "Allergy",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#0047FF" }} className="font-medium">
+            {row.original.allergy}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("status", {
+        header: "Status",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#2e263dd3" }}>
+            {row.original.status}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("description", {
+        header: "Description",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#2e263dd3" }}>
+            {row.original.description}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("last_updated", {
+        header: "Last Updated",
+        cell: ({ row }) => <Typography>{row.original.last_updated}</Typography>,
+      }),
+      columnHelper.accessor("action", {
+        header: "Action",
+        cell: ({ row }) => {
+          // const action = row.original.action;
+
+          return (
+            <div className="flex gap-2">
+              <FaEye className="cursor-pointer" />
+              <FaPen
+                className="
+                 cursor-pointer"
+              />
+            </div>
+          );
+        },
+      }),
+    ],
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: { rowSelection },
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Card>
+      <div className="p-4 flex justify-between items-center">
+        <CardHeader title="Allergies" />
+        <Button variant="outlined" color="inherit">
+          +CREATE
+        </Button>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className={tableStyles.table}>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Card>
+  );
+};
+
+export default AllergiesTable;
