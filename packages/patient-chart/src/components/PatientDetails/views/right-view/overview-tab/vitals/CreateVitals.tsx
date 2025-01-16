054f876ed89312b87e51668b22d3348afddba493
tanishaxcaliber <tanisha.shrivas@xcaliber.health>
2025-01-16 20:35:42 +0530

    ""

--- packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/vitals/CreateVitals.tsx
+++ packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/vitals/CreateVitals.tsx
@@ -178,8 +178,8 @@ export default function CreateVitals({ title, patientId }: CreateVitalsProps) {
     };
 
     try {
-      // await VitalService.createVitals(vitalsPayload);
-      console.log("Vitals created successfully.", vitalsPayload);
+      await VitalService.createVitals(vitalsPayload);
+      // console.log("Vitals created successfully.", vitalsPayload);
       setIsOpen(false);
     } catch (error) {
       console.error("Error creating vitals:", error);
