971910a120c7b138c1a96b9646aa15e0ca8ae079
tanishaxcaliber <tanisha.shrivas@xcaliber.health>
2025-01-16 20:34:32 +0530

    [TS]: Implemented create problem functionality

--- packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/OverviewTab.tsx
+++ packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/OverviewTab.tsx
@@ -5,7 +5,7 @@ import Grid from "@mui/material/Grid";
 import AllergiesTable from "./AllergiesDetails";
 import ImmunizationsTable from "./ImmunizationDetails";
 import MedicationsTable from "./MedicationsDetails";
-import ProblemsTable from "./ProblemDetails";
+import ProblemsTable from "./problems/ProblemDetails";
 import VitalsTable from "./VitalDetails";
 
 // const getData = async () => {
---
+++ packages/patient-chart/src/components/PatientDetails/views/right-view/overview-tab/problems/ProblemDetails.tsx
@@ -0,0 +1,208 @@
+// React Imports
+import { useEffect, useMemo, useState } from "react";
+
+// Mui Imports
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Typography from "@mui/material/Typography";
+import TablePagination from "@mui/material/TablePagination";
+
+// Third-party Imports
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import tableStyles from "@core/styles/table.module.css";
+import { fetchProblems } from "../utils/getPatientProblems";
+import { CreateProblem } from "./CreateProblem";
+
+// React Icons
+import { FaEye, FaPen } from "react-icons/fa";
+
+export interface ProblemProps {
+  problem: string;
+  status: string;
+  description: string;
+  last_updated: string;
+  action: string;
+}
+
+const ProblemsTable = ({ id }: { id?: string }) => {
+  const [rowSelection, setRowSelection] = useState({});
+  const [data, setData] = useState<ProblemProps[]>([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const updateProblemsState = (createdProblemData) => {
+    setData([...data, createdProblemData]);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchProblems(id);
+        setData(response);
+      } catch (error) {
+        console.error("Error fetching problems:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const columnHelper = createColumnHelper<ProblemProps>();
+
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("problem", {
+        header: "Problem",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#0047FF" }} className="font-medium">
+            {row.original.problem}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("status", {
+        header: "Status",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#2e263dd3" }}>
+            {row.original.status}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("description", {
+        header: "Description",
+        cell: ({ row }) => (
+          <Typography sx={{ color: "#2e263dd3" }}>
+            {row.original.description}
+          </Typography>
+        ),
+      }),
+      columnHelper.accessor("last_updated", {
+        header: "Last Updated",
+        cell: ({ row }) => <Typography>{row.original.last_updated}</Typography>,
+      }),
+      columnHelper.accessor("action", {
+        header: "Action",
+        cell: ({ row }) => {
+          // const action = row.original.action;
+
+          return (
+            <div className="flex gap-2">
+              <FaEye className="cursor-pointer" />
+              <FaPen
+                className="
+                 cursor-pointer"
+              />
+            </div>
+          );
+        },
+      }),
+    ],
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      rowSelection,
+    },
+    initialState: {
+      pagination: {
+        pageSize: 5,
+      },
+    },
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  // Pagination handlers
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleRowsPerPageChange = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0); // Reset to the first page when rows per page change
+  };
+
+  return (
+    <>
+      <Card>
+        <div className="p-4 flex justify-between items-center">
+          <CardHeader title="Problems" />
+          <CreateProblem patientId={id} updateProblems={updateProblemsState} />
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className={tableStyles.table}>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            {table.getFilteredRowModel().rows.length === 0 ? (
+              <tbody>
+                <tr>
+                  <td
+                    colSpan={table.getVisibleFlatColumns().length}
+                    className="text-center"
+                  >
+                    No data available
+                  </td>
+                </tr>
+              </tbody>
+            ) : (
+              <tbody>
+                {table.getRowModel().rows.map((row) => (
+                  <tr key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            )}
+          </table>
+        </div>
+
+        <TablePagination
+          component="div"
+          count={data.length}
+          page={page}
+          onPageChange={handlePageChange}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleRowsPerPageChange}
+          rowsPerPageOptions={[7, 10, 25]}
+        />
+      </Card>
+    </>
+  );
+};
+
+export default ProblemsTable;
