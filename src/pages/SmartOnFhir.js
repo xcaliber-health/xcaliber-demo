057a25f32cbcbe2c5eec5edc45e3be780c922709
Razi <raziuddin@xcaliber.heatlh>
2025-02-18 21:40:03 +0530

    fix: Add code challenge, fix cors

--- src/pages/SmartOnFhir.js
+++ src/pages/SmartOnFhir.js
@@ -11,30 +11,40 @@ const SmartOnFhir = () => {
   const launchValue = searchParams.get("launch");
 
   useEffect(() => {
-		const launch = async () => {
-    await axios
-      .get(
-        "https://api.preview.platform.athenahealth.com/oauth2/v1/authorize",
-        {
-          params: {
-            client_id: "0oaw7snv22DdD8hP5297",
-            response_type: "code",
-            redirect_uri: "https://dev-demo.xcaliberhealth.ai/fhir-app",
-            scope:
-              "launch user/AllergyIntolerance.read user/Appointment.read user/Assessment.read user/Binary.read user/CarePlan.read user/CareTeam.read user/ClinicalImpression.read user/Condition.read user/Device.read user/DiagnosticReport.read user/DocumentReference.read user/Encounter.read user/Goal.read user/Immunization.read user/Location.read user/MedicalAdministration.read user/Medication.read user/MedicationDispense.read user/MedicationOrder.read user/MedicationRequest.read user/MedicationStatement.read user/Observation.read user/Organization.read user/Patient.read user/PracticeConfiguration.read user/Practitioner.read user/Procedure.read user/Provenance.read user/Provider.read",
-            aud: iss,
-            launch: launchValue,
-            state: "12345",
-          },
-        }
-      ) // Update with actual backend URL
-      .then((response) => {
-				console.log("Received data from authorize ep - ", response.data);
-        setData(response.data);
-      })
-      .catch((error) => setError(error.message));
-		};
-		launch();
+    const launch = async () => {
+      const CLIENT_ID = "0oaw7snv22DdD8hP5297";
+      const REDIRECT_URI = "https://dev-demo.xcaliberhealth.ai/fhir-app";
+      const AUTH_URL = `https://api.preview.platform.athenahealth.com/oauth2/v1/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
+        REDIRECT_URI
+      )}&response_type=code&scope=launch user/AllergyIntolerance.read user/Appointment.read user/Assessment.read user/Binary.read user/CarePlan.read user/CareTeam.read user/ClinicalImpression.read user/Condition.read user/Device.read user/DiagnosticReport.read user/DocumentReference.read user/Encounter.read user/Goal.read user/Immunization.read user/Location.read user/MedicalAdministration.read user/Medication.read user/MedicationDispense.read user/MedicationOrder.read user/MedicationRequest.read user/MedicationStatement.read user/Observation.read user/Organization.read user/Patient.read user/PracticeConfiguration.read user/Practitioner.read user/Procedure.read user/Provenance.read user/Provider.read&aud=${iss}&launch=${launchValue}&state=12345&code_challenge=qjrzSW9gMiUgpUvqgEPE4_-8swvyCtfOVvg55o5S_es&code_challenge_method=S256`;
+      const loginWithAthena = () => {
+        window.location.href = AUTH_URL; // Redirect user to Athena login
+      };
+			loginWithAthena();
+    //   await axios
+    //     .get(
+    //       "https://api.preview.platform.athenahealth.com/oauth2/v1/authorize",
+    //       {
+    //         withCredentials: true,
+    //         params: {
+    //           client_id: "0oaw7snv22DdD8hP5297",
+    //           response_type: "code",
+    //           redirect_uri: "https://dev-demo.xcaliberhealth.ai/fhir-app",
+    //           scope:
+    //             "launch user/AllergyIntolerance.read user/Appointment.read user/Assessment.read user/Binary.read user/CarePlan.read user/CareTeam.read user/ClinicalImpression.read user/Condition.read user/Device.read user/DiagnosticReport.read user/DocumentReference.read user/Encounter.read user/Goal.read user/Immunization.read user/Location.read user/MedicalAdministration.read user/Medication.read user/MedicationDispense.read user/MedicationOrder.read user/MedicationRequest.read user/MedicationStatement.read user/Observation.read user/Organization.read user/Patient.read user/PracticeConfiguration.read user/Practitioner.read user/Procedure.read user/Provenance.read user/Provider.read",
+    //           aud: iss,
+    //           launch: launchValue,
+    //           state: "12345",
+    //         },
+    //       }
+    //     ) // Update with actual backend URL
+    //     .then((response) => {
+    //       console.log("Received data from authorize ep - ", response.data);
+    //       setData(response.data);
+    //     })
+    //     .catch((error) => setError(error.message));
+    };
+    launch();
   }, [iss, launchValue]);
 
   return (
