fbb0c3694fc3ba9231c6ae54a7f9b1d76de90f8b
Lasya-Kondapally04 <https://github.com/Lasya-Kondapally04>
2025-09-24 17:03:39 +0530

    Changes in build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     open: "/scheduling/find", // 👈 dev server will open this route
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
