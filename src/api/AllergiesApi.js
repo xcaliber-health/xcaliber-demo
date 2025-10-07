a49071e6c3f553d5aa29fb292615da9b056fe5ab
Lasya-Kondapally04 <https://github.com/Lasya-Kondapally04>
2025-10-07 10:13:14 +0530

    Clinical Document attachments

--- src/api/AllergiesApi.js
+++ src/api/AllergiesApi.js
@@ -9,42 +9,7 @@ export async function fetchAllergies(patientId, sourceId,departmentId) {
   });
 }
 
-// ✅ Create a new allergy
-// export async function createAllergy(patientId, sourceId,departmentId, values) {
-//   const now = new Date().toISOString();
 
-//   const allergy = {
-//     resourceType: "AllergyIntolerance",
-//     clinicalStatus: {
-//       coding: [
-//         { system: "http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical", code: values.status || "active" },
-//       ],
-//     },
-//     verificationStatus: {
-//       coding: [
-//         { system: "http://terminology.hl7.org/CodeSystem/allergyintolerance-verification", code: "unconfirmed" },
-//       ],
-//     },
-//     type: "allergy",
-//     category: ["food", "medication", "environment", "other"],
-//     patient: { reference: `Patient/${patientId}` },
-//     code: { text: values.allergy },
-//     reaction: [
-//       {
-//         description: values.reaction,
-//         severity: values.severity,
-//         onset: values.onsetDate || now,
-//       },
-//     ],
-//   };
-
-//   return fhirFetch(`/AllergyIntolerance?patient=${patientId}&departmentId=${departmentId}`, {
-//     sourceId,
-//     method: "POST",
-//     headers: { "Content-Type": "application/fhir+json" },
-//     body: JSON.stringify(allergy),
-//   });
-// }
 // ✅ Create a new allergy (all user-entered values)
 export async function createAllergy(patientId, sourceId, departmentId, values) {
   const now = new Date().toISOString();
